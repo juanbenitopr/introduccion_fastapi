a718f2ef0b937ca4b298f662f6ff43b51bc62e21
Juan Benito <juanbenito.pacheco@smartprotection.com>
2021-08-08 18:16:47 +0200

    2.5 Validando los query params

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from typing import Dict, Any
 
 import uvicorn
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
@@ -24,8 +25,13 @@ PEOPLE = {
 
 
 @app.get('/{name}', status_code=201)
-def validate_name(name: NameTypes = Path(..., description='name of the person you want to retrieve the info')):
-    return PEOPLE[name]
+def validate_name(age: int = Query(10, ge=10, le=100, title='Person Age', description='possible age of the person'),
+                  name: NameTypes = Path(..., title='Person Name',
+                                         description='name of the person you want to retrieve the info'),
+                  ) -> Dict[str, Any]:
+    response = {**PEOPLE[name], 'age_right': PEOPLE[name]['age'] == age}
+
+    return response
 
 
 if __name__ == '__main__':
