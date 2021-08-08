79adfccc00a2a3225bb2f8942c6af361d6f8675e
Juan Benito <juanbenito.pacheco@smartprotection.com>
2021-08-08 17:38:19 +0200

    2.2 Creando nuestro primer endpoint

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 # Your code here
+@app.get('/{id}', status_code=201)
+def hello_world(id: int, parameter: str = 'hola'):
+    return {'content': 'hello world', 'id': id, 'param': parameter}
 
 
 if __name__ == '__main__':
