bab44539592e37c56969f1684446c8017bb12159
Juan Benito <juanbenito.pacheco@smartprotection.com>
2021-08-19 19:32:41 +0200

    4.5 Dependencias vs Middleware

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from time import time
 from typing import List
 
 import uvicorn
 from fastapi import FastAPI, Depends, Header, HTTPException
 from pydantic import BaseModel, Field
+from starlette.requests import Request
 
 app = FastAPI()
 
@@ -36,6 +38,7 @@ def validate_request(token: str = Header(...)):
     if token != 'Bearer':
         raise HTTPException(status_code=401, detail='Token Invalid')
 
+
 @app.get('/student', response_model=List[Student], dependencies=[Depends(validate_request)])
 def list_students(student_filter: StudentFilter = Depends()) -> List[Student]:
     students = [
@@ -45,6 +48,14 @@ def list_students(student_filter: StudentFilter = Depends()) -> List[Student]:
     ]
     return [student for student in students if StudentFilter(age=student.age, name=student.name) == student_filter]
 
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time()
+    response = await call_next(request)
+    process_time = time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app', reload=True)
