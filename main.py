e19640cf3bcd0b81ee996b5026c4bc702acbe1ee
Juan Benito <juanbenito.pacheco@smartprotection.com>
2021-08-19 18:47:32 +0200

    4.2 Usando Dependencias

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from typing import List
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -14,6 +16,20 @@ class Student(BaseModel):
 @app.post('/student', response_model=Student, response_model_exclude={'id'})
 def create_stuent(student: Student) -> Student:
     return student
+
+
+def get_student_name(name: str) -> str:
+    return name
+
+
+@app.get('/student', response_model=List[Student])
+def list_students(name: str = Depends(get_student_name)) -> List[Student]:
+    students = [
+        Student(id=1, name='Juan', age=3),
+        Student(id=2, name='Antonio', age=10),
+        Student(id=3, name='Alberto', age=12),
+    ]
+    return [student for student in students if student.name == name]
 
 
 if __name__ == '__main__':
